f76293ad1a0e5c4ac4dc8763856549fdc76c60ba
Костин Владимир Валерьевич <vladimir.kostin@vitech.team>
2025-03-14 15:06:25 +0300

    Добавлена миграция

--- config/config.go
+++ config/config.go
@@ -1,13 +1,20 @@
 package config
 
 import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
 	"log/slog"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/pkg/errors"
 )
+
+import _ "github.com/lib/pq"
 
 // Config конфигурации приложения.
 type Config struct {
@@ -19,10 +26,15 @@ type Config struct {
 	HTTP struct {
 		Port int `default:"8000" envconfig:"PORT"`
 	}
+	DB struct {
+		Connection    *sql.DB
+		TYPE          string `default:"" envconfig:"DB_TYPE"`
+		ConnectionDsn string `default:"" envconfig:"CONNECTION_DSN"`
+	}
 }
 
 // LoadConfig функция для загрузки конфигурации.
-func LoadConfig(cfg interface{}, prefix string) error {
+func LoadConfig(cfg *Config, prefix string) error {
 	if err := godotenv.Load(); err != nil && !errors.Is(err, os.ErrNotExist) {
 		return errors.Wrap(err, "не удалось создать конфиг")
 	}
@@ -30,6 +42,22 @@ func LoadConfig(cfg interface{}, prefix string) error {
 	if err := envconfig.Process(prefix, cfg); err != nil {
 		return errors.Wrap(err, "ошибка обработки env-переменных")
 	}
+
+	var err error
+	cfg.DB.Connection, err = sql.Open(cfg.DB.TYPE, cfg.DB.ConnectionDsn)
+	if err != nil {
+		log.Fatalf("failed to connect to db: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*1)
+	defer cancel()
+
+	err = cfg.DB.Connection.PingContext(ctx)
+	fmt.Println(err)
+	if err != nil {
+		log.Fatalf("failed to ping to db: %v", err)
+	}
+	fmt.Println(cfg.DB.ConnectionDsn)
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gorilla/handlers v1.5.2
 	github.com/joho/godotenv v1.5.1
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/lib/pq v1.10.9
 	github.com/mash/go-accesslog v1.3.0
 	github.com/pkg/errors v0.9.1
 	github.com/rs/zerolog v1.33.0
@@ -17,5 +18,5 @@ require (
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
 )
